b929be99a262e12125d82b02c8b528fabfce2a5a
m <mmarszik@gmail.com>
2020-03-29 14:08:15 +0200

    .

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
 verb='silent'
 seed='12345678'
-tasks='1'
-loops='20000000'
+loops='100000000'
+tasks='21000'
 
-echo "time ./OpenCLSumRnd $verb OpenCL $seed $tasks $loops"
-time ./OpenCLSumRnd $verb OpenCL $seed $tasks $loops
+echo "time ./OpenCLSumRnd $verb OpenCL $seed $loops $tasks"
+time ./OpenCLSumRnd $verb OpenCL $seed $loops $tasks
 
-echo "time ./OpenCLSumRnd $verb OpenMP $seed $tasks $loops"
-time ./OpenCLSumRnd $verb OpenMP $seed $tasks $loops
+tasks='18'
+echo "time ./OpenCLSumRnd $verb OpenMP $seed $loops $tasks"
+time ./OpenCLSumRnd $verb OpenMP $seed $loops $tasks
 
-#echo "time ./OpenCLSumRnd $verb OneThread $seed $tasks $loops"
-#time ./OpenCLSumRnd $verb OneThread $seed $tasks $loops
+echo "time ./OpenCLSumRnd $verb OneThread $seed $loops $tasks"
+time ./OpenCLSumRnd $verb OneThread $seed $loops $tasks
 
 
